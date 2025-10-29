0a2bf60f84b5d6f16818945df0b7dd30650ca2fa
cwd0217 <cwd0217@naver.com>
2025-10-29 23:59:24 +0900

    feat: To Do 기본 화면
    To Do 리스트의가 없는 화면
    home_page의 위젯 UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,17 +8,15 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       //appBar
       appBar: AppBar(
-        title: Text("지윤's Tasks",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
+        title: Text(
+          "지윤's Tasks",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
       ),
       //body
       body: Container(
         alignment: Alignment.topCenter,
-        // width: 400,
+
         height: 250,
         margin: EdgeInsets.all(20),
         padding: EdgeInsets.all(20),
@@ -28,30 +26,32 @@ class HomePage extends StatelessWidget {
         ),
         child: Column(
           children: [
-            
-            Image.asset("assets/no_todo.webp",
-            width: 100,
-            height: 100,
+            //[이미지]
+            Image.asset("assets/no_todo.webp", width: 100, height: 100),
+            SizedBox(height: 12),
+            //[첫번째 텍스트]
+            Text(
+              "아직 할 일이 없음",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 12),
-            Text("아직 할 일이 없음",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            SizedBox(height: 12),
-            Text("할 일을 추가하고 지윤's Tasks에서\n할 일을 추적하세요.",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 14,
-              height: 1.5,
-            ),
+            //[두번쨰 텍스트]
+            Text(
+              "할 일을 추가하고 지윤's Tasks에서\n할 일을 추적하세요.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 14, height: 1.5),
             ),
           ],
         ),
       ),
-      
+      // [floating버튼]
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: CircleBorder(),
+        backgroundColor: Colors.deepOrange,
+
+        child: Icon(Icons.add, color: Colors.white, size: 24),
+      ),
     );
   }
 }

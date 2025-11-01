eaacf3ef49b01e077f537ca79941cc6a5e737185
cwd0217 <cwd0217@naver.com>
2025-11-01 21:17:43 +0900

    docs: 홈페이지 이동
    홈페이지 기능들을 가시성을 위해서 새로운 폴더를 만들어서 이동시킴

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tasks/main.dart';
+import 'package:tasks/page/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
